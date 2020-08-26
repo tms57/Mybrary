a5cee7b74f43b1759e94f460dc88c26e5ab0ae7d
Ted <ted.shapera@gmail.com>
2020-08-26 15:34:57 -0500

    Fix dotenv must use config() not parse()

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 // require('dotenv').load() is now deprecated (shocking)
 if (process.env.NODE_ENV !== 'production') {
-  // require('dotenv').config();
-  require('dotenv').parse();
-} // @16.35
+  require('dotenv').config();
+}
 
 const express = require('express');
 const app = express();
